c2f95917024d318e0071aa1d6f77400fdca402bd
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-22 16:29:42 +0900

    프로젝트_유저 엔티티 생성

---
+++ src/main/java/com/ssoggong/stonemanager_server/entity/ProjectUserId.java
@@ -0,0 +1,9 @@
+package com.ssoggong.stonemanager_server.entity;
+
+import java.io.Serializable;
+
+public class ProjectUserId implements Serializable {
+
+    private String member;
+    private String product;
+}
