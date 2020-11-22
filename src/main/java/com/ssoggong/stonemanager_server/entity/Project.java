6fa27109e89294330e92de777eb887201708e003
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-22 16:29:23 +0900

    프로젝트 엔티티 생성

---
+++ src/main/java/com/ssoggong/stonemanager_server/entity/Project.java
@@ -0,0 +1,68 @@
+package com.ssoggong.stonemanager_server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Project {
+
+    @Id @GeneratedValue
+    @Column(name = "project_idx")
+    private Long idx;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<ProjectUser> projectUserList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<File> fileList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<CheckList> checkListList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Task> taskList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Schedule> scheduleList = new ArrayList<>();
+
+    private String name;
+
+    //== 연관관계 메서드 ==//
+    public void addProjectUser(ProjectUser projectUser) {
+        projectUserList.add(projectUser);
+        projectUser.setProject(this);
+    }
+
+    public void addComment(Comment comment){
+        commentList.add(comment);
+        comment.setProject(this);
+    }
+
+    public void addFile(File file) {
+        fileList.add(file);
+        file.setProject(this);
+    }
+
+    public void addCheckList(CheckList checkList){
+        checkListList.add(checkList);
+        checkList.setProject(this);
+    }
+
+    public void addTask(Task task){
+        taskList.add(task);
+        task.setProject(this);
+    }
+
+    public void addSchedule(Schedule schedule){
+        scheduleList.add(schedule);
+        schedule.setProject(this);
+    }
+}
