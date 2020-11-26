32c7e623bb9f797c8c6f9b84f65941ef0dd7c613
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-26 21:06:45 +0900

    checkList -> checklist 변경

--- src/main/java/com/ssoggong/stonemanager_server/entity/Checklist.java
+++ src/main/java/com/ssoggong/stonemanager_server/entity/Checklist.java
@@ -14,8 +14,8 @@ public class Checklist {
     private Long idx;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "task_idx")
-    private Task task;
+    @JoinColumn(name = "project_idx")
+    private Project project;
 
     private String name;
     private Boolean state; // 타입 확인 필요(Enum)
