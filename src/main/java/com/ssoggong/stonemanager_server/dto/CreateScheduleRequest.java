074b89a22deeeabf6766b23978aa9efdf3449feb
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-27 19:19:30 +0900

    일정 추가 api 구현

---
+++ src/main/java/com/ssoggong/stonemanager_server/dto/CreateScheduleRequest.java
@@ -0,0 +1,22 @@
+package com.ssoggong.stonemanager_server.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateScheduleRequest {
+    private String scheduleName;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime scheduleDate;
+    private String scheduleDescription;
+    private List<Long> scheduleAssigneeIdList;
+    private List<Long> scheduleTagIdList;
+}
--- src/main/java/com/ssoggong/stonemanager_server/service/ScheduleService.java
+++ src/main/java/com/ssoggong/stonemanager_server/service/ScheduleService.java
@@ -1,10 +1,20 @@
 package com.ssoggong.stonemanager_server.service;
 
-import com.ssoggong.stonemanager_server.entity.Schedule;
+import com.ssoggong.stonemanager_server.dto.CreateScheduleRequest;
+import com.ssoggong.stonemanager_server.entity.*;
+import com.ssoggong.stonemanager_server.exception.ScheduleTagNotFoundException;
+import com.ssoggong.stonemanager_server.exception.UserNotFoundException;
+import com.ssoggong.stonemanager_server.repository.ProjectRepository;
 import com.ssoggong.stonemanager_server.repository.ScheduleRepository;
+import com.ssoggong.stonemanager_server.repository.ScheduleTagRepository;
+import com.ssoggong.stonemanager_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,7 +22,50 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+    private final ScheduleTagRepository scheduleTagRepository;
+
 
     @Transactional
     public void saveSchedule(Schedule schedule) { scheduleRepository.save(schedule); }
+
+    @Transactional
+    public void createSchedule(Long userId, Long projectId, CreateScheduleRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        List<Long> assigneeIdList =  request.getScheduleAssigneeIdList();
+        List<Long> scheduleTagIdList = request.getScheduleTagIdList();
+
+        Set<UserSchedule> userScheduleSet = new HashSet<>();
+        for(Long assigneeId : assigneeIdList) {
+            User assignee = userRepository.findById(assigneeId).orElseThrow(UserNotFoundException::new);
+            UserSchedule userSchedule = UserSchedule.builder()
+                    .user(assignee)
+                    .build();
+
+            userScheduleSet.add(userSchedule);
+        }
+
+        Set<ScheduleScheduleTag> scheduleScheduleTagSet = new HashSet<>();
+        for(Long scheduleTagId: scheduleTagIdList) {
+            ScheduleTag scheduleTag = scheduleTagRepository.findById(scheduleTagId).orElseThrow(ScheduleTagNotFoundException::new);
+            ScheduleScheduleTag scheduleScheduleTag = ScheduleScheduleTag.builder()
+                    .scheduleTag(scheduleTag)
+                    .build();
+
+            scheduleScheduleTagSet.add(scheduleScheduleTag);
+        }
+
+        Project project = projectRepository.findById(projectId).orElseThrow();
+        Schedule schedule = Schedule.builder()
+                .name(request.getScheduleName())
+                .date(request.getScheduleDate())
+                .description(request.getScheduleDescription())
+                .userScheduleSet(userScheduleSet)
+                .scheduleScheduleTagSet(scheduleScheduleTagSet)
+                .project(project)
+                .build();
+
+        saveSchedule(schedule);
+    }
 }
