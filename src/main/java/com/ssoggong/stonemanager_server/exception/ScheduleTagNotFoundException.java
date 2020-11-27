06d6136d04f0143725a870f1670359aa9729d557
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-27 19:19:42 +0900

    예외 추가

---
+++ src/main/java/com/ssoggong/stonemanager_server/exception/ScheduleTagNotFoundException.java
@@ -0,0 +1,5 @@
+package com.ssoggong.stonemanager_server.exception;
+
+public class ScheduleTagNotFoundException extends RuntimeException {
+    public ScheduleTagNotFoundException() { super("Invalid ScheduleTagId Exception");}
+}
