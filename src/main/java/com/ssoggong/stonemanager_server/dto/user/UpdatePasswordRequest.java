e3f0321d394edb37bb165f1964c39add69425bc9
gowoon-choi <gowoonvv@gmail.com>
2020-11-30 07:11:35 +0900

    feat: #47 비밀번호 변경

---
+++ src/main/java/com/ssoggong/stonemanager_server/dto/user/UpdatePasswordRequest.java
@@ -0,0 +1,15 @@
+package com.ssoggong.stonemanager_server.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePasswordRequest {
+    private String password;
+    private String newPassword;
+}
