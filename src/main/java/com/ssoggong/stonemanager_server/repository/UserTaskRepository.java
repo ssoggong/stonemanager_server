511ac8972ac8a86b34909c67f3d73898f8103d75
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-29 18:00:40 +0900

    feat: #70 할 일 조회 (필터링) 완료

--- src/main/java/com/ssoggong/stonemanager_server/repository/UserTaskRepository.java
+++ src/main/java/com/ssoggong/stonemanager_server/repository/UserTaskRepository.java
@@ -1,7 +1,13 @@
 package com.ssoggong.stonemanager_server.repository;
 
+import com.ssoggong.stonemanager_server.entity.Task;
+import com.ssoggong.stonemanager_server.entity.User;
 import com.ssoggong.stonemanager_server.entity.UserTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserTaskRepository extends JpaRepository<UserTask, Long> {
+
+    List<UserTask> findAllByUser(User user);
 }
