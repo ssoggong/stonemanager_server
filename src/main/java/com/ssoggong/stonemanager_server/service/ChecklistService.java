6c82ba0ece9debaa1587137fff035347dd572194
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-29 19:12:45 +0900

    feat: #74 체크리스트 생성 완료

--- src/main/java/com/ssoggong/stonemanager_server/service/ChecklistService.java
+++ src/main/java/com/ssoggong/stonemanager_server/service/ChecklistService.java
@@ -1,8 +1,15 @@
 package com.ssoggong.stonemanager_server.service;
 
-import com.ssoggong.stonemanager_server.entity.Checklist;
+import com.ssoggong.stonemanager_server.dto.checklist.ChecklistRequest;
+import com.ssoggong.stonemanager_server.entity.*;
+import com.ssoggong.stonemanager_server.exception.ProjectNotFoundException;
+import com.ssoggong.stonemanager_server.exception.TaskTagNotFoundException;
+import com.ssoggong.stonemanager_server.exception.UserNotFoundException;
 import com.ssoggong.stonemanager_server.repository.ChecklistRepository;
+import com.ssoggong.stonemanager_server.repository.ProjectRepository;
+import com.ssoggong.stonemanager_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Check;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,4 +22,14 @@ public class ChecklistService {
 
     @Transactional
     public void saveChecklist(Checklist checklist) { checklistRepository.save(checklist); }
+
+    @Transactional
+    public void createChecklist(Task task, ChecklistRequest checklistRequest) {
+        Checklist checklist = Checklist.builder()
+                .name(checklistRequest.getChecklistName())
+                .state(checklistRequest.getChecklistState())
+                .task(task)
+                .build();
+        saveChecklist(checklist);
+    }
 }
