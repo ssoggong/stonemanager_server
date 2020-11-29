c7573ff5301b616fe27d7d94619ef18d3c167ef4
gowoon-choi <gowoonvv@gmail.com>
2020-11-30 06:07:49 +0900

    feat: #43 프로필사진 변경 완료

---
+++ src/main/java/com/ssoggong/stonemanager_server/dto/user/UserProfileDto.java
@@ -0,0 +1,14 @@
+package com.ssoggong.stonemanager_server.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileDto {
+    String userProfileImage;
+}
--- src/main/java/com/ssoggong/stonemanager_server/entity/User.java
+++ src/main/java/com/ssoggong/stonemanager_server/entity/User.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
