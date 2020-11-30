915a1c92c6dc3772fdac4099f83fb312dcd0412c
gowoon-choi <gowoonvv@gmail.com>
2020-11-30 09:30:59 +0900

    feat:#104 팀원 초대 수락

--- src/main/java/com/ssoggong/stonemanager_server/service/ProjectUserService.java
+++ src/main/java/com/ssoggong/stonemanager_server/service/ProjectUserService.java
@@ -1,6 +1,8 @@
 package com.ssoggong.stonemanager_server.service;
 
+import com.ssoggong.stonemanager_server.entity.Project;
 import com.ssoggong.stonemanager_server.entity.ProjectUser;
+import com.ssoggong.stonemanager_server.entity.User;
 import com.ssoggong.stonemanager_server.exception.MultipleNotFoundException;
 import com.ssoggong.stonemanager_server.exception.NotFoundException;
 import com.ssoggong.stonemanager_server.repository.ProjectUserRepository;
@@ -21,5 +23,14 @@ public class ProjectUserService {
     @Transactional
     public void deleteProjectUser(Long userId, Long projectId){
         if(projectUserRepository.deleteProjectUserByUserAndProject(userId, projectId) == 0) throw new MultipleNotFoundException();
+    }
+
+    @Transactional
+    public void createProjectUser(User user, Project project){
+        ProjectUser projectUser = ProjectUser.builder()
+                .user(user)
+                .project(project)
+                .build();
+        saveProjectUser(projectUser);
     }
 }
